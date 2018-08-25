2ff296fc6c4bd92caf9417ab1afe41cbe852ff2f
babbaj <babbaj45@gmail.com>
2018-08-25 05:16:13 -0400

    smh didnt add the task class

---
+++ src/main/java/net/futureclient/asmlib/GenMappingsTask.java
@@ -0,0 +1,30 @@
+package net.futureclient.asmlib;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Task;
+import org.gradle.api.tasks.TaskAction;
+
+import java.io.File;
+import java.util.Objects;
+
+// the task responsible for parsing the .srg files and generating a json file for remapping
+public class GenMappingsTask extends DefaultTask {
+
+    private final Task genSrgs;
+
+    public GenMappingsTask() {
+        this.genSrgs = getProject().getTasks().findByName("genSrgs");
+        Objects.requireNonNull(genSrgs);
+    }
+
+    @TaskAction
+    public void runTask() {
+        // TODO: properly check if these properties/files exist
+        final File mapToNotch = (File)genSrgs.property("mcpToNotch");
+        final File mcpToSrg =   (File)genSrgs.property("mcpToSrg");
+
+        System.out.println("GenMappingsTask: " + mapToNotch);
+        System.out.println("GenMappingsTask: " + mcpToSrg);
+
+    }
+}
