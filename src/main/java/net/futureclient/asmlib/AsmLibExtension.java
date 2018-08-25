23669911f634989fe94b0c410a9f5815a3b1b1b6
0x22 <9968651+0-x-2-2@users.noreply.github.com>
2018-08-25 05:15:20 -0400

    Epic Extension. Example: https://gist.github.com/0-x-2-2/2e22e07a483306b30c9c555261251ad0

--- src/main/java/net/futureclient/asmlib/AsmLibExtension.java
+++ src/main/java/net/futureclient/asmlib/AsmLibExtension.java
@@ -1,13 +1,23 @@
 package net.futureclient.asmlib;
 
 import org.gradle.api.Project;
+import org.gradle.api.tasks.SourceSet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class AsmLibExtension {
 
-    private Project project;
+    private List<SourceSet> asmLibSourceSets = new ArrayList<>();
+    private List<String> asmLibMappingConfigs = new ArrayList<>();
 
     public AsmLibExtension(Project project) {
-        this.project = project;
-        this.project.afterEvaluate(p -> System.out.println("epic"));
+        //TODO: init
+    }
+
+    public void add(SourceSet sourceSet, String... asmLibMappingConfigs) {
+        this.asmLibSourceSets.add(sourceSet);
+        this.asmLibMappingConfigs.addAll(Arrays.asList(asmLibMappingConfigs));
     }
 }
