e26f3c115f5ab9b5bbc5b35082bbf729c39c292f
babbaj <babbaj45@gmail.com>
2018-08-31 04:16:54 -0400

    fixed method notch names not being in json

--- src/main/java/net/futureclient/asmlib/AsmLibExtension.java
+++ src/main/java/net/futureclient/asmlib/AsmLibExtension.java
@@ -1,5 +1,6 @@
 package net.futureclient.asmlib;
 
+import com.google.gson.*;
 import net.futureclient.asmlib.forgegradle.ForgeGradleVersion;
 import net.futureclient.asmlib.parser.srg.SrgMap;
 import net.futureclient.asmlib.parser.srg.SrgParser;
@@ -17,11 +18,16 @@ import java.util.*;
 
 class AsmLibExtension {
 
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     private final Project project;
     private final ForgeGradleVersion forgeGradleVersion;
 
     private Set<SourceSet> asmLibSourceSets = new HashSet<>();
     private Set<String> asmLibMappingConfigs = new HashSet<>();
+
+    private File mcpToNotch;
+    private File mcpToSrg;
 
     public AsmLibExtension(Project project, ForgeGradleVersion forgeGradleVersion) {
         this.project = project;
@@ -45,8 +51,6 @@ class AsmLibExtension {
         Path testFile = tempDir.resolve("test");
 
         compileTask.doFirst(task -> {
-            File mcpToNotch;
-            File mcpToSrg;
 
             switch (this.forgeGradleVersion) {
                 case FORGEGRADLE_2_X:
@@ -63,25 +67,86 @@ class AsmLibExtension {
 
             System.out.println("GenMappingsTask: " + mcpToNotch);
             System.out.println("GenMappingsTask: " + mcpToSrg);
-
-            final SrgMap mcpToNotchMap = parseSrgFile(mcpToNotch);
-
-            System.out.println(mcpToNotchMap.getMethodMap().size());
-
-            final SrgMap mcpToSrgMap = parseSrgFile(mcpToSrg);
-
-
-            System.out.println(mcpToSrgMap.getMethodMap().size());
         });
 
         compileTask.doLast(task -> {
             System.out.println("gonna put file here");
+
+            final String json = serializeJson(parseSrgFile(mcpToNotch), parseSrgFile(mcpToSrg));
             try {
-                Files.write(testFile, "hello world".getBytes(), StandardOpenOption.CREATE);
+                System.out.println("writing the file...");
+                Files.write(testFile, json.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         });
+    }
+
+    private String serializeJson(SrgMap mcpToNotch, SrgMap mcpToSrg) {
+        System.out.println("Serializing json");
+        final JsonObject root = new JsonObject();
+        {
+            final JsonObject mappings = new JsonObject();
+            root.add("mappings", mappings);
+            // map mcp name to notch name
+            final Map<String, String> classNames = mcpToNotch.getClassMap();
+            System.out.println(classNames.size() + " classes");
+
+            // TODO: This REALLY needs to be optimized
+            classNames.forEach((mcpClass, obfClass) -> {
+                final JsonObject classJson = new JsonObject();
+                mappings.add(mcpClass, classJson);
+
+                classJson.add("notch", new JsonPrimitive(obfClass));
+                {
+                    final JsonObject fields = new JsonObject();
+                    classJson.add("fields", fields);
+
+
+                    mcpToNotch.getFieldMap().entrySet().stream()
+                            .filter(entry -> entry.getKey().getParentClass().equals(mcpClass))
+                            .forEach(entry -> {
+                        JsonObject fieldValues = new JsonObject();
+                        fieldValues.addProperty("notch", entry.getValue());
+                        fields.add(entry.getKey().getName(), fieldValues);
+                    });
+                    mcpToSrg.getFieldMap().entrySet().stream()
+                            .filter(entry -> entry.getKey().getParentClass().equals(mcpClass))
+                            .forEach(entry -> {
+                                JsonObject fieldValues = Optional.ofNullable(fields.get(entry.getKey().getName()))
+                                        .map(JsonElement::getAsJsonObject)
+                                        .orElseThrow(() -> new IllegalStateException("Missing obf field: " + entry.getKey().getName())); // TODO: check for missing srg fields
+                                fieldValues.addProperty("srg", entry.getValue());
+                                fields.add(entry.getKey().getName(), fieldValues);
+                            });
+
+                }
+                {
+                    final JsonObject methods = new JsonObject();
+                    classJson.add("methods", methods);
+
+                    mcpToNotch.getMethods().entrySet().stream()
+                            .filter(entry -> entry.getKey().getParentClass().equals(mcpClass))
+                            .forEach(entry -> {
+                                JsonObject methodValues = new JsonObject();
+                                methodValues.addProperty("notch", entry.getValue());
+                                methods.add(entry.getKey().getMcpName() + entry.getKey().getSignature(), methodValues);
+                            });
+                    mcpToSrg.getMethods().entrySet().stream()
+                            .filter(entry -> entry.getKey().getParentClass().equals(mcpClass))
+                            .forEach(entry -> {
+                                JsonObject methodValues = Optional.ofNullable(methods.get(entry.getKey().getMcpName() + entry.getKey().getSignature()))
+                                        .map(JsonElement::getAsJsonObject)
+                                        .orElseThrow(() -> new IllegalStateException("Missing obf field"));
+                                methodValues.addProperty("searge", entry.getValue());
+                                methods.add(entry.getKey().getMcpName() + entry.getKey().getSignature(), methodValues);
+                            });
+                }
+
+            });
+
+        }
+        return GSON.toJson(root);
     }
 
     private SrgMap parseSrgFile(File file) {
